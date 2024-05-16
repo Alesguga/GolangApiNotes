44a30e5ff8f51fdd2b6de91d2b652ba358db0494
Alejandro Gutiérrez Galán <alexgutigalan04@gmail.com>
2024-05-16 08:47:07 +0200

    2 comm

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/gin-contrib/cors"
 	"log"
 	"net/http"
 
@@ -21,7 +22,6 @@ var client *db.Client
 
 func main() {
 	ctx := context.Background()
-	// Usa la ruta a tu archivo de clave de cuenta de servicio
 	sa := option.WithCredentialsFile("notesasus-firebase-adminsdk-86cpl-d4a6fc676d.json")
 
 	app, err := firebase.NewApp(ctx, nil, sa)
@@ -29,7 +29,6 @@ func main() {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	// Inicializa el cliente de la base de datos con la URL correcta
 	client, err = app.DatabaseWithURL(ctx, "https://notesasus-default-rtdb.firebaseio.com/")
 	if err != nil {
 		log.Fatalf("error initializing database client: %v\n", err)
@@ -37,26 +36,36 @@ func main() {
 
 	router := gin.Default()
 
-	// Rutas para manejar las notas
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"} // Cambia esto según sea necesario
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Type"}
+	router.Use(cors.New(config))
+
 	router.POST("/notes", createNote)
 	router.GET("/notes", getNotes)
 	router.GET("/notes/:id", getNote)
 	router.PUT("/notes/:id", updateNote)
 	router.DELETE("/notes/:id", deleteNote)
 
-	router.Run(":8080")
+	err = router.Run(":8000")
+	if err != nil {
+		return
+	}
 }
 
 func createNote(c *gin.Context) {
 	var note Note
 	if err := c.ShouldBindJSON(&note); err != nil {
+		log.Printf("Error binding JSON: %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	log.Printf("Received note: %+v", note)
 
-	// Crear una nueva referencia en la base de datos y asignar un ID a la nota
 	ref, err := client.NewRef("notes").Push(context.Background(), nil)
 	if err != nil {
+		log.Printf("Error creating reference: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -76,6 +85,7 @@ func createNote(c *gin.Context) {
 func getNotes(c *gin.Context) {
 	var notes map[string]Note
 	if err := client.NewRef("notes").Get(context.Background(), &notes); err != nil {
+		log.Printf("Error getting notes: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -87,6 +97,7 @@ func getNote(c *gin.Context) {
 	id := c.Param("id")
 	var note Note
 	if err := client.NewRef("notes/"+id).Get(context.Background(), &note); err != nil {
+		log.Printf("Error getting note: %v", err)
 		c.JSON(http.StatusNotFound, gin.H{"error": "Note not found"})
 		return
 	}
@@ -98,11 +109,13 @@ func updateNote(c *gin.Context) {
 	id := c.Param("id")
 	var note Note
 	if err := c.ShouldBindJSON(&note); err != nil {
+		log.Printf("Error binding JSON: %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := client.NewRef("notes/"+id).Set(context.Background(), note); err != nil {
+		log.Printf("Error setting note in database: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -113,6 +126,7 @@ func updateNote(c *gin.Context) {
 func deleteNote(c *gin.Context) {
 	id := c.Param("id")
 	if err := client.NewRef("notes/" + id).Delete(context.Background()); err != nil {
+		log.Printf("Error deleting note: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
