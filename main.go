cac4738f292ac544be03ee9e0e090d7d9359d115
Alejandro Gutiérrez Galán <alexgutigalan04@gmail.com>
2024-06-16 18:58:13 +0200

    2 comm

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 
 	"firebase.google.com/go"
 	"firebase.google.com/go/db"
-	"github.com/gin-gonic/gin"
 	"google.golang.org/api/option"
 )
 
@@ -21,15 +24,39 @@ type Note struct {
 var client *db.Client
 
 func main() {
-	ctx := context.Background()
-	sa := option.WithCredentialsFile("notesasus-firebase-adminsdk-86cpl-d4a6fc676d.json")
+	// Cargar variables de entorno desde el archivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
-	app, err := firebase.NewApp(ctx, nil, sa)
+	ctx := context.Background()
+	creds := map[string]string{
+		"type":                        os.Getenv("FIREBASE_TYPE"),
+		"project_id":                  os.Getenv("FIREBASE_PROJECT_ID"),
+		"private_key_id":              os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
+		"private_key":                 os.Getenv("FIREBASE_PRIVATE_KEY"),
+		"client_email":                os.Getenv("FIREBASE_CLIENT_EMAIL"),
+		"client_id":                   os.Getenv("FIREBASE_CLIENT_ID"),
+		"auth_uri":                    os.Getenv("FIREBASE_AUTH_URI"),
+		"token_uri":                   os.Getenv("FIREBASE_TOKEN_URI"),
+		"auth_provider_x509_cert_url": os.Getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+		"client_x509_cert_url":        os.Getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+	}
+
+	credsJSON, err := json.Marshal(creds)
+	if err != nil {
+		log.Fatalf("error marshalling creds: %v", err)
+	}
+
+	opt := option.WithCredentialsJSON(credsJSON)
+
+	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	client, err = app.DatabaseWithURL(ctx, "https://notesasus-default-rtdb.firebaseio.com/")
+	client, err = app.DatabaseWithURL(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("error initializing database client: %v\n", err)
 	}
@@ -37,9 +64,9 @@ func main() {
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"} // Cambia esto según sea necesario
+	config.AllowOrigins = []string{"*"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
-	config.AllowHeaders = []string{"Origin", "Content-Type"}
+	config.AllowHeaders = []string{"*"}
 	router.Use(cors.New(config))
 
 	router.POST("/notes", createNote)
